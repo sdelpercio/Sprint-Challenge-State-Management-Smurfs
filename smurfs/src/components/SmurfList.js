108d5b3442a45bd3435dc83c270b3a1946753183
Shawn DelPercio <sdelperc@gmail.com>
2020-02-21 13:11:01 -0500

    finished styling

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,24 +1,51 @@
 import React, { useContext } from 'react';
-
 import { SmurfContext } from '../contexts';
 
-const SmurfList = () => {
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	cardBox: {
+		width: '100%',
+		marginTop: '4rem',
+		display: 'flex',
+		justifyContent: 'space-evenly',
+		alignItems: 'space-evenly'
+	},
+	aCard: {
+		width: '20%',
+		height: 'auto',
+		textAlign: 'center',
+		background: 'rgb(143,205,255)',
+		background:
+			'linear-gradient(325deg, rgba(143,205,255,0.6029762246695554) 50%, rgba(0,0,0,0.6001751042213761) 100%)'
+	}
+});
+
+const SmurfList = props => {
 	const { smurfs } = useContext(SmurfContext);
 
+	const { classes } = props;
+
 	return (
-		<>
+		<Box className={classes.cardBox}>
 			{smurfs.map(item => (
-				<div key={item.id}>
-					<h3>{item.name}</h3>
-					<h6>
-						Age: <span>{item.age}</span>
-					</h6>
-					<h6>
-						Height: <span>{item.height}</span>
-					</h6>
-				</div>
+				<Card className={classes.aCard} key={item.id}>
+					<CardContent>
+						<Typography variant='h6'>{item.name}</Typography>
+						<Typography variant='body2'>
+							Age: <span>{item.age}</span>
+						</Typography>
+						<Typography variant='body2'>
+							Height: <span>{item.height}</span>
+						</Typography>
+					</CardContent>
+				</Card>
 			))}
-		</>
+		</Box>
 	);
 };
-export default SmurfList;
+export default withStyles(styles)(SmurfList);
